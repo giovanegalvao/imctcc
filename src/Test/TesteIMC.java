1a1ed7f5c36b7056dadf41661659541be64f4bd7
Giovane Galvao <giovane_galvao@hotmail.com.br>
2014-04-08 20:45:56 -0700

    09/04

--- src/Test/TesteIMC.java
+++ src/Test/TesteIMC.java
@@ -1,32 +1,82 @@
 package Test;
 
-import com.thoughtworks.selenium.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteIMC {
-	private Selenium selenium;
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-	@Before
-	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://localhost/imc/");
-		selenium.start();
-	}
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "/home/giovane/Downloads/chromedriver");  
+    driver = new ChromeDriver();
+    baseUrl = "http://localhost/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-	@Test
-	public void testEIMC() throws Exception {
-		selenium.open("/imc/");
-		selenium.type("id=altura", "1.81");
-		selenium.type("id=peso", "85");
-		selenium.click("id=calcular");
-		selenium.waitForPageToLoad("30000");
-	}
+  @Test
+  public void testEWEBDRIVEChrome() throws Exception {
+    driver.get(baseUrl + "/imc/");
+    driver.findElement(By.id("altura")).clear();
+    driver.findElement(By.id("altura")).sendKeys("1.81");
+    driver.findElement(By.id("peso")).clear();
+    driver.findElement(By.id("peso")).sendKeys("85");
+    driver.findElement(By.id("calcular")).click();
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		//selenium.stop();
-	}
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
